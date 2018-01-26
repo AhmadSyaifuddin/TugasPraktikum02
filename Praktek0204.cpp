af97a41fa116215ac16a22bf68880a83d6907f56
AhmadSyaifuddin <syaifuddinahmad598@gmail.com>
2018-01-26 18:03:13 +0800

    Praktikum02

---
+++ Praktek0204.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+	double luasRmh=42;
+	double parket=193*1215;
+	double luasBox = parket*8/1000000;
+	
+	cout << "Luas Rumah 		: "<<luasRmh<<" m2"<<endl;
+	cout << "Luas 1 Box Parket	: "<<luasBox<<" m2"<<endl;
+	
+	double jumlahBox, bayarParket, bayarPasang, totalBayar;
+	jumlahBox = luasRmh/luasBox;
+	cout <<"jumlah Box Parket 	: "<<jumlahBox<<endl;
+	
+	bayarParket = jumlahBox*500;
+	cout <<"Biaya Parket (Rp) 	: "<<bayarParket<<endl;
+	
+	bayarPasang = luasRmh*20;
+	cout <<"Biaya Pasang (Rp) 	: "<<bayarPasang<<endl;
+	
+	totalBayar = bayarParket+bayarPasang;
+	cout <<"Total Biaya  (Rp) 	: "<<totalBayar<<endl;
+	
+}
